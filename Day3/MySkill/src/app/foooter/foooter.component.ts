e3cfa56371e6d89b37cb639de7a60647ecab3982
Surendra <surendrabankar218@gmail.com>
2022-04-25 14:04:22 +0530

    changes

--- Day3/MySkill/src/app/foooter/foooter.component.ts
+++ Day3/MySkill/src/app/foooter/foooter.component.ts
@@ -3,7 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-foooter',
   templateUrl: './foooter.component.html',
-  styleUrls: ['./foooter.component.css']
+  styles: [`
+
+  footer {
+    text-align: center;
+    background-color: rgb(135, 173, 179);
+    color: rgb(11, 3, 3);
+    padding: 3px;
+  }
+  
+  `]
 })
 export class FoooterComponent implements OnInit {
 
--- Day3/MySkill/src/app/header/header.component.ts
+++ Day3/MySkill/src/app/header/header.component.ts
@@ -3,7 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styles: [`
+  .container{
+    clear:both;
+    overflow:hidden;
+    margin-left:15px ;
+  }
+  h2 {
+      float: left;
+      color: rgb(1, 1, 25);
+      font-size: 28px;
+  }
+  nav{
+    float:left;
+  }
+  
+  ul li{
+    display: inline-block; padding:10px;
+    font-size:20px; font-family:raleway;
+  }
+  ul li:hover{
+    color:rgb(16, 43, 224);
+    
+  }
+  `]
 })
 export class HeaderComponent implements OnInit {
 
